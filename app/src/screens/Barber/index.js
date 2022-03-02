b1f82674adcc005a922c5a87bc9db3e0c4ccc54c
karolsouzaq@gmail.com <karolsouzaq@gmail.com>
2022-03-02 19:58:51 -0300

    Criado modal para agendamento.

--- app/src/screens/Barber/index.js
+++ app/src/screens/Barber/index.js
@@ -1,159 +1,179 @@
-import React, { useState, useEffect } from "react";
-import { useNavigation, useRoute } from "@react-navigation/native";
-import Swiper from 'react-native-swiper'
+import React, { useState, useEffect } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Swiper from 'react-native-swiper';
 import {
-  Container,
-  Scroller,
-  SwipeDot,
-  SwipeDotActive,
-  SwipeItem,
-  SwipeImage,
-  FakeSwiper,
-  PageBody,
-  UserInfoArea,
-  ServiceArea,
-  TestimonialArea,
-  TestimonialItem,
-  TestimonialInfo,
-  TestimonialName,
-  TestimonialBody,
+	Container,
+	Scroller,
+	SwipeDot,
+	SwipeDotActive,
+	SwipeItem,
+	SwipeImage,
+	FakeSwiper,
+	PageBody,
+	UserInfoArea,
+	ServiceArea,
+	TestimonialArea,
+	TestimonialItem,
+	TestimonialInfo,
+	TestimonialName,
+	TestimonialBody,
+	UserAvatar,
+	UserInfo,
+	UserInfoName,
+	UserFavButton,
+	BackButton,
+	LoadingIcon,
+	ServicesTitle,
+	ServiceItem,
+	ServiceInfo,
+	ServiceName,
+	ServicePrice,
+	ServiceChooseButton,
+	ServiceChooseBtnText
+} from './styles';
 
-  UserAvatar,
-  UserInfo,
-  UserInfoName,
-  UserFavButton,
-  BackButton,
-  LoadingIcon,
-  ServicesTitle,
-  ServiceItem,
-  ServiceInfo,
-  ServiceName,
-  ServicePrice,
-  ServiceChooseButton,
-  ServiceChooseBtnText
-} from "./styles";
+import Api from '../../Api';
+import Stars from '../../components/Stars';
+import BarberModal from "../../components/BarberModal";
 
-import Api from "../../Api";
-import Stars from "../../components/Stars";
-import FavoriteIcon from "../../assets/favorite.svg";
-import BackIcon from "../../assets/back.svg";
-import NavPrevIcon from "../../assets/nav_prev.svg";
-import NavNextIcon from "../../assets/nav_next.svg";
+import FavoriteIcon from '../../assets/favorite.svg';
+import FavoriteFullIcon from '../../assets/favorite_full.svg';
+import BackIcon from '../../assets/back.svg';
+import NavPrevIcon from '../../assets/nav_prev.svg';
+import NavNextIcon from '../../assets/nav_next.svg';
 
 export default () => {
-  const navigation = useNavigation();
-  const route = useRoute();
+	const navigation = useNavigation();
+	const route = useRoute();
 
-  const [userInfo, setUserInfo] = useState({
-    id: route.params.id,
-    avatar: route.params.avatar,
-    name: route.params.name,
-    stars: route.params.stars
-  });
+	const [ userInfo, setUserInfo ] = useState({
+		id: route.params.id,
+		avatar: route.params.avatar,
+		name: route.params.name,
+		stars: route.params.stars
+	});
 
-  const [loading, setLoading] = useState(false);
+	const [ loading, setLoading ] = useState(false);
+	const [ favorited, setFavorited ] = useState(false);
+	const [ selectedService, setSelectedService ] = useState(null);
+	const [ showModal, setShowModal ] = useState(false);
 
-  useEffect(() => {
-    const getBarberInfo = async () => {
-      setLoading(true);
-      let json = await Api.getBarber(userInfo.id);
-      if (!json.error) {
-        setUserInfo(json.data);
+	useEffect(() => {
+		const getBarberInfo = async () => {
+			setLoading(true);
+			let json = await Api.getBarber(userInfo.id);
+			if (!json.error) {
+				setUserInfo(json.data);
+				setFavorited(json.data.favorited);
+			} else {
+				alert('Erro: ' + json.error);
+			}
+			setLoading(false);
+		};
+		getBarberInfo();
+	}, []);
 
-      } else {
-        alert("Erro: " + json.error);
-      }
-      setLoading(false);
-    }
-    getBarberInfo();
-  }, []);
+	const handleBackButton = () => {
+		navigation.goBack();
+	};
 
-  const handleBackButton = () => {
-    navigation.goBack();
-  }
+	const handleFavClick = () => {
+		setFavorited(!favorited);
+		Api.setFavorite(userInfo.id);
+	};
 
-  return (
-    <Container>
-      <Scroller>
-        {userInfo.photos && userInfo.photos.length > 0 ?
-          <Swiper
-            style={{ height: 240 }}
-            dot={<SwipeDot />}
-            activeDot={<SwipeDotActive />}
-            paginationStyle={{ top: 15, right: 15, bottom: null, left: null }}
-            autoplay={true}
-          >
-            {userInfo.photos.map((item, key) => (
-              <SwipeItem key={key}>
-                <SwipeImage source={{ uri: item.url }} resizeMode="cover" />
-              </SwipeItem>
-            ))}
-          </Swiper>
-          :
-          <FakeSwiper>
-          </FakeSwiper>
-        }
-        <PageBody>
-          <UserInfoArea>
-            <UserAvatar source={{ uri: userInfo.avatar }} />
-            <UserInfo>
-              <UserInfoName>{userInfo.name}</UserInfoName>
-              <Stars stars={userInfo.stars} showNumber={true} />
-            </UserInfo>
-            <UserFavButton>
-              <FavoriteIcon widht="24" height="24" fill="#FF0000" />
-            </UserFavButton>
-          </UserInfoArea>
-          {loading &&
-            <LoadingIcon size="large" color="#000000" />
-          }
-          {userInfo.services && (
-            <ServiceArea>
-              <ServicesTitle>Lista de serviços</ServicesTitle>
-              {userInfo.services.map((item, key) => (
-                <ServiceItem key={key}>
-                  <ServiceInfo>
-                    <ServiceName>{item.name}</ServiceName>
-                    <ServicePrice>R$ {item.price}</ServicePrice>
-                  </ServiceInfo>
-                  <ServiceChooseButton>
-                    <ServiceChooseBtnText>Agendar</ServiceChooseBtnText>
-                  </ServiceChooseButton>
-                </ServiceItem>
-              )
-              )}
-            </ServiceArea>
-          )}
+	const handleServiceChoose = (key) => {
+		setSelectedService(key);
+		setShowModal(true);
+	};
 
-          {userInfo.testimonials && userInfo.testimonials.length > 0 &&
+	return (
+		<Container>
+			<Scroller>
+				{userInfo.photos && userInfo.photos.length > 0 ? (
+					<Swiper
+						style={{ height: 240 }}
+						dot={<SwipeDot />}
+						activeDot={<SwipeDotActive />}
+						paginationStyle={{ top: 15, right: 15, bottom: null, left: null }}
+						autoplay={true}
+					>
+						{userInfo.photos.map((item, key) => (
+							<SwipeItem key={key}>
+								<SwipeImage source={{ uri: item.url }} resizeMode="cover" />
+							</SwipeItem>
+						))}
+					</Swiper>
+				) : (
+					<FakeSwiper />
+				)}
+				<PageBody>
+					<UserInfoArea>
+						<UserAvatar source={{ uri: userInfo.avatar }} />
+						<UserInfo>
+							<UserInfoName>{userInfo.name}</UserInfoName>
+							<Stars stars={userInfo.stars} showNumber={true} />
+						</UserInfo>
+						<UserFavButton onPress={handleFavClick}>
+							{favorited ? (
+								<FavoriteFullIcon widht="24" height="24" fill="#FF0000" />
+							) : (
+								<FavoriteIcon widht="24" height="24" fill="#FF0000" />
+							)}
+						</UserFavButton>
+					</UserInfoArea>
+					{loading && <LoadingIcon size="large" color="#000000" />}
+					{userInfo.services && (
+						<ServiceArea>
+							<ServicesTitle>Lista de serviços</ServicesTitle>
+							{userInfo.services.map((item, key) => (
+								<ServiceItem key={key}>
+									<ServiceInfo>
+										<ServiceName>{item.name}</ServiceName>
+										<ServicePrice>R$ {item.price.toFixed(2)}</ServicePrice>
+									</ServiceInfo>
+									<ServiceChooseButton onPress={() => handleServiceChoose(key)}>
+										<ServiceChooseBtnText>Agendar</ServiceChooseBtnText>
+									</ServiceChooseButton>
+								</ServiceItem>
+							))}
+						</ServiceArea>
+					)}
 
-            <TestimonialArea>
-              <Swiper
-                style={{ height: 110 }}
-                showsPagination={false}
-                showsButtons={true}
-                prevButton={<NavPrevIcon widht="35" height="35" fill="#000000" />}
-                nextButton={<NavNextIcon widht="35" height="35" fill="#000000" />}
-              >
-                {userInfo.testimonials.map((item, key) => (
-                  <TestimonialItem key={key}>
-                    <TestimonialInfo>
-                      <TestimonialName>{item.name} </TestimonialName>
-                      <Stars stars={item.rate} showNumber={false} />
-                    </TestimonialInfo>
-                    <TestimonialBody>{item.body} </TestimonialBody>
-                  </TestimonialItem>
-                )
-                )}
-              </Swiper>
-            </TestimonialArea>
-          }
+					{userInfo.testimonials &&
+					userInfo.testimonials.length > 0 && (
+						<TestimonialArea>
+							<Swiper
+								style={{ height: 110 }}
+								showsPagination={false}
+								showsButtons={true}
+								prevButton={<NavPrevIcon widht="35" height="35" fill="#000000" />}
+								nextButton={<NavNextIcon widht="35" height="35" fill="#000000" />}
+							>
+								{userInfo.testimonials.map((item, key) => (
+									<TestimonialItem key={key}>
+										<TestimonialInfo>
+											<TestimonialName>{item.name} </TestimonialName>
+											<Stars stars={item.rate} showNumber={false} />
+										</TestimonialInfo>
+										<TestimonialBody>{item.body} </TestimonialBody>
+									</TestimonialItem>
+								))}
+							</Swiper>
+						</TestimonialArea>
+					)}
+				</PageBody>
+			</Scroller>
+			<BackButton onPress={handleBackButton}>
+				<BackIcon width="44" height="44" fill="#FFFFFF" />
+			</BackButton>
 
-        </PageBody>
-      </Scroller>
-      <BackButton onPress={handleBackButton}>
-        <BackIcon width="44" height="44" fill="#FFFFFF" />
-      </BackButton>
-    </Container >
-  );
-}
+      <BarberModal 
+      show={showModal}
+      setShow={setShowModal}
+      user={userInfo}
+      service={selectedService}
+      />
+		</Container>
+	);
+};
--- app/src/screens/Barber/styles.js
+++ app/src/screens/Barber/styles.js
@@ -101,17 +101,35 @@ margin-top:50px;
 
 export const TestimonialArea = styled.View`
 margin-top: 30px;
+margin-bottom: 50px;
 `;
 
 export const TestimonialItem = styled.View`
-background-color: #268596
+background-color: #268596;
+padding: 15px;
+border-radius: 10px;
+height: 110px;
+justify-content: center;
+margin-left: 50px;
+margin-right: 50px;
 `;
 
-export const TestimonialInfo = styled.View``;
+export const TestimonialInfo = styled.View`
+flex-direction: row;
+justify-content: space-between;
+margin-bottom: 5px;
+`;
 
-export const TestimonialName = styled.Text``;
+export const TestimonialName = styled.Text`
+font-size: 14px;
+font-weight: bold;
+color: #FFFFFF;
+`;
 
-export const TestimonialBody = styled.Text ``;
+export const TestimonialBody = styled.Text `
+font-size: 13px;
+color: #FFFFFF;
+`;
 
 export const ServiceArea = styled.View`
 margin-top: 30px;
